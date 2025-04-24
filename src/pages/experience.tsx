f6162a3e0b50e889548b6b9f750055e67375b852
jklaaste <jklaaste@gmail.com>
2025-04-24 21:32:14 +0200

    chore

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout'
+import experience from '../../experience.json'
 
 export default function About() {
   return (
     <Layout>
-      'Empty'
+      {
+        experience.experience.map(exp => {
+          return (
+            <div className=''>
+              <div>{exp.company}</div>
+              <div>{exp.company}</div>
+              <div>{exp.company}</div>
+            </div>
+          )
+        })
+      }
     </Layout>
   );
 }
