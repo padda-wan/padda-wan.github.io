54fd5a6447fa35859d82950ce0edbc2b7f61742d
jklaaste <jklaaste@gmail.com>
2025-04-26 17:35:31 +0200

    chore: exit animations

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { usePathname } from "next/navigation";
 import { Bebas_Neue } from "next/font/google";
 import Image from "next/image";
-import { AnimatePresence, motion } from 'motion/react'
+import { AnimatePresence, LayoutGroup, motion } from 'motion/react'
 import "@/styles/globals.css";
 import { useEffect, useState } from 'react';
 
@@ -32,33 +32,46 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${bebasNeu.variable}`}>
         <div className='grid grid-cols-5 grid-rows-5 auto-rows-auto auto-cols-auto h-screen'>
-          <AnimatePresence mode="wait" initial={true}>
-            <motion.div
-              className='z-1 col-start-1 col-end-6 row-start-1 row-end-6 border-t-4'
-              layout
-              style={{ gridRowStart: transition ? '5' : '1' }}
-              exit={{ gridRowStart: transition ? '5' : '1' }}
-            ></motion.div>
-            <motion.div
-              className='bg-white col-start-1 col-end-6 row-start-1 row-end-6'
-              layout
-              style={{ gridRowStart: transition ? '5' : '1' }}
-            ></motion.div>
-            <motion.div
-              className='z-1 col-start-1 col-end-6 row-start-1 row-end-6 border-r-4'
-              layout
-              style={{ gridColumnEnd: transition ? '1' : '6' }}
-            ></motion.div>
-            <motion.div
-              className='bg-white col-start-1 col-end-6 row-start-1 row-end-6'
-              layout
-              style={{ gridColumnEnd: transition ? '1' : '6' }}
-            ></motion.div>
-            <main className='col-start-2 col-end-5 row-start-2 row-end-5 border-t-4 border-r-4 p-20 overflow-y-auto'>
-              {children}
-            </main>
-            <div></div>
-          </AnimatePresence>
+          <LayoutGroup>
+            <AnimatePresence mode="wait" initial={true}>
+              <motion.div
+                className='z-2 col-start-1 col-end-6 row-start-1 row-end-6 border-t-4'
+                layout
+                style={{ gridRowStart: transition ? '5' : '1' }}
+                exit={{ gridRowStart: transition ? '1' : '5' }}
+              ></motion.div>
+              <motion.div
+                className='bg-white col-start-1 col-end-6 row-start-1 row-end-6'
+                layout
+                style={{ gridRowStart: transition ? '5' : '1' }}
+                exit={{ gridRowStart: transition ? '5' : '5' }}
+              ></motion.div>
+              <motion.div
+                className='z-2 col-start-1 col-end-6 row-start-1 row-end-6 border-r-4'
+                layout
+                style={{ gridColumnEnd: transition ? '1' : '6' }}
+                exit={{ gridColumnEnd: transition ? '6' : '1' }}
+              ></motion.div>
+              <motion.div
+                className='bg-white col-start-1 col-end-6 row-start-1 row-end-6'
+                layout
+                style={{ gridColumnEnd: transition ? '1' : '6' }}
+                exit={{ gridColumnEnd: transition ? '6' : '1' }}
+              ></motion.div>
+              <main className='col-start-2 col-end-5 row-start-2 row-end-5 border-t-4 border-r-4 p-20 overflow-y-auto'>
+                {children}
+              </main>
+              <div className='col-start-2 col-end-3 row-start-5 row-end-5 -x-20 z-3'>
+                {
+                  path !== '/' ? (
+                    <Link href={'/'}>Back</Link>
+                  ) : (
+                    ''
+                  )
+                }
+              </div>
+            </AnimatePresence>
+          </LayoutGroup>
         </div>
       </body>
     </html >
