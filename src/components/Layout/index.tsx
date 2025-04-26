d34860f61efb24eb43b91fff4c0ffed9ef95ebe9
jklaaste <jklaaste@gmail.com>
2025-04-26 16:43:06 +0200

    feat: animation is working

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -12,6 +12,11 @@ const bebasNeu = Bebas_Neue({
   subsets: ["latin"],
 });
 
+const LineTransition = {
+  default: { ease: "linear" },
+  layout: { duration: 0.3 }
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,21 +33,28 @@ export default function RootLayout({
       <body className={`${bebasNeu.variable}`}>
         <div className='grid grid-cols-5 grid-rows-5 auto-rows-auto auto-cols-auto h-screen'>
           <motion.div
-            // initial grid-row-start: 1
-            // animate grid-row-start: 5
+            className='z-1 col-start-1 col-end-6 row-start-1 row-end-6 border-t-4'
+            layout
+            style={{ gridRowStart: transition ? '5' : '1' }}
+          ></motion.div>
+          <motion.div
             className='bg-white col-start-1 col-end-6 row-start-1 row-end-6 border-t-4'
             layout
             style={{ gridRowStart: transition ? '5' : '1' }}
+          ></motion.div>
+          <motion.div
+            className='z-1 col-start-1 col-end-6 row-start-1 row-end-6 border-r-4'
+            layout
+            style={{ gridColumnEnd: transition ? '1' : '6' }}
           ></motion.div>
           <motion.div
             className='bg-white col-start-1 col-end-6 row-start-1 row-end-6 border-r-4'
             layout
             style={{ gridColumnEnd: transition ? '1' : '6' }}
           ></motion.div>
-          <div className='col-start-2 col-end-5 row-start-2 row-end-5 border-t-4 border-r-4 p-4'>
+          <main className='col-start-2 col-end-5 row-start-2 row-end-5 border-t-4 border-r-4 p-4'>
             ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum ante nulla, nec facilisis diam ultricies id. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse finibus lectus vitae quam placerat sodales. Nam eget tellus non odio faucibus tempus. Sed venenatis tellus porta dignissim iaculis. Vivamus sit amet magna erat. Donec sagittis in diam et efficitur. In elementum, erat ac viverra elementum, risus lectus viverra nulla, et molestie eros ipsum in mauris. Morbi elementum at odio a fermentum. In non erat dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer convallis congue molestie. Sed ut mauris in ex interdum cursus. Ut sodales ac tellus quis tincidunt. Ut eget ante at velit vulputate pulvinar vitae nec urna.
-
-          </div>
+          </main>
         </div>
       </body>
     </html >
